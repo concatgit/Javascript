2f185a8931a5d9d3d0d20fc4a8578dd10eb32827
Bằng <107402166+concatgit@users.noreply.github.com>
2023-01-11 11:32:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+
+function hexToTen(hex)
+{   
+        let total=0;
+        for(let i=0;i<hex.length;i++)
+        {
+            let index;
+            if(checkNumber(hex[i]))
+            {
+                index=Number(hex[i]);
+                total=total*16+index;
+                console.log(total);
+            }else
+            {
+                switch(hex[i])
+                {
+                    case 'A':
+                        index=10;
+                        break;
+                    case 'B':
+                        index=11;
+                        break;
+                    case 'C':
+                        index=12;
+                        break;
+                    case 'D':
+                        index=13;
+                        break;
+                    case 'E':
+                        index=14;
+                        break;
+                    case 'F':
+                        index=15;
+                        break;
+                }
+                total=total*16+index;
+                console.log(total);
+            }
+        }
+ 
+        return total;
+}
+function checkNumber(str)
+{
+    if(typeof str!="string") return false;
+    return !isNaN(str) && !isNaN(parseFloat(str));   // !isNaN(parseFloat(str)) de xoa khoang trang trong xau
+}
+function toBinary(decimal)
+{
+    var pdu;
+    var arr=[];
+    var result=0;
+    var i=0;
+    while(decimal>0)
+    {
+        pdu=decimal%2;
+        decimal=parseInt(decimal/2);
+        arr[i]=pdu;  
+        i++;
+    }
+    for(var j=i-1;j>=0;j--)
+    {
+        result=result*10+arr[j];
+    }
+    console.log(`Kết quả thu được là:${result}`);
+}
+let ten=hexToTen("BA3");
+console.log(ten);
+toBinary(ten);
